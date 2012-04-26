6ba4aa6e5ef619767a1df1b0fe52037ba39ee002
Day Davis Waterbury <day@azimuthonline.com>
2012-04-26 11:10:38 -0700

    Basic Rails 3.2.3 sans Test Unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c1e4c859f49aff77abadd7de22369f58ae7caac85e48af3364f97aaa1ef5b788f179626ef16f33be7e8943f907ee75edba555efed6d168055be2c85c4e104a3e'
