5aeb7968c364414b6eb2de3fc115d57681e86822
Day Davis Waterbury <day@azimuthonline.com>
2012-04-26 12:40:58 -0700

    FAILED: Static pages About page should have the content 'About Us'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,16 @@ describe "Static pages" do
   
   describe "Help page" do
 
-      it "should have the content 'Help'" do
-        visit '/static_pages/help'
-        page.should have_content('Help')
-      end
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
     end
+  end
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
 end
